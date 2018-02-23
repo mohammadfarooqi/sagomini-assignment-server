85f3c9c31cdbb157f798f98773acabb7167eb7c1
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-02-22 22:02:22 -0500

    added validation for bundle route

--- web/routes/validators/bundles.js
+++ web/routes/validators/bundles.js
@@ -2,17 +2,24 @@ const { Joi } = require('celebrate');
 
 // read
 const read = {
-
+  params: {
+    bundle_id: Joi.string().required()
+  }
 };
 
 // set
 const set = {
-
+  params: {
+    bundle_id: Joi.string().required(),
+    new_build_number: Joi.number().integer().required()
+  }
 };
 
 // bump
 const bump = {
-
+  params: {
+    bundle_id: Joi.string().required()
+  }
 };
 
 module.exports = {
